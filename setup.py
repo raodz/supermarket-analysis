42ffc033c70938f0f05d08863d9f26afca1aec81
Rafal Odziemski <raodziem@gmail.com>
2023-10-10 14:39:10 +0200

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name='supermarket_analysis',
+    version='0.0.1',
+    author='raodz',
+    author_email='raodziem@gmail.com',
+    description='Exploratory data analysis of supermarket chain\'s sales dataset',
+    url='https://github.com/raodz/supermarket-analysis',
+    # project_urls = {
+    #     "Bug Tracker": "https://github.com/mike-huls/toolbox/issues"
+    # },  # kaggle?
+    license='MIT',
+    packages=['supermarket_analysis'],
+    install_requires=['requests'],
+)
